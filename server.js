9593415c39df1c231b36d103e94d98686e3388b2
Akshat Trivedi <akshattriv.iit@gmail.com>
2020-10-31 01:54:36 +0530

    Improved error handeling

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ process.on('uncaughtException', (err) => {
 const DB = process.env.DATABASE.replace(
   '<password>',
   process.env.DB_PASS
-).replace('<dbname>', 'blogtest');
+).replace('<dbname>', 'blog');
 
 mongoose
   .connect(DB, {
